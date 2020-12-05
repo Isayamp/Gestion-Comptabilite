d1873dfbb52133b44091e265fe796dd80447b9f3
Isayamp <Isayamlelapombo@gmail.com>
2020-12-05 15:52:57 +0100

    Add layout components files

---
+++ inc/sidebar.php
@@ -0,0 +1,39 @@
+<div id="sidebar-collapse" class="col-sm-3 col-lg-2 sidebar">
+		<div class="profile-sidebar">
+			<div class="profile-userpic">
+				<img src="http://placehold.it/50/30a5ff/fff" class="img-responsive" alt="">
+			</div>
+			<div class="profile-usertitle">
+				<div class="profile-usertitle-name">Username</div>
+				<div class="profile-usertitle-status"><span class="indicator label-success"></span>Online</div>
+			</div>
+			<div class="clear"></div>
+		</div>
+		<div class="divider"></div>
+		<ul class="nav menu">
+			<li class="active"><a href="index.php"><em class="fa fa-home">&nbsp;</em> Accueil</a></li>
+			<li><a href="pages/clients.php"><em class="fa fa-users">&nbsp;</em> Clients</a></li>
+			<li><a href="pages/entrees.php"><em class="fa fa-bar-input">&nbsp;</em> Entrées</a></li>
+			<li><a href="pages/sorties.php"><em class="fa fa-output">&nbsp;</em> Sorties</a></li>
+			<li><a href="pages/retours.php"><em class="fa fa-output">&nbsp;</em> Retours</a></li>
+			<li><a href="pages/caisse.php"><em class="fa fa-database">&nbsp;</em> Solde</a></li>
+			<li class="parent "><a data-toggle="collapse" href="#sub-item-1">
+				<em class="fa fa-navicon">&nbsp;</em> Autres <span data-toggle="collapse" href="#sub-item-1" class="icon pull-right"><em class="fa fa-plus"></em></span>
+				</a>
+				<ul class="children collapse" id="sub-item-1">
+					<li><a class="" href="pages/departements.php">
+						<span class="fa fa-arrow-right">&nbsp;</span> Departements
+					</a></li>
+					<li><a class="" href="#">
+						<span class="fa fa-arrow-right">&nbsp;</span> Services vendus
+					</a></li>
+					<li><a class="" href="#">
+						<span class="fa fa-arrow-right">&nbsp;</span> Pays
+                    </a></li>
+                    <li><a class="" href="#">
+						<span class="fa fa-arrow-right">&nbsp;</span> Provinces
+					</a></li>
+				</ul>
+			</li>
+		</ul>
+	</div><!--/.sidebar-->
